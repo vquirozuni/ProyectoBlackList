936a326d243c6f799174b82e8c559bd42b7e5d2a
rarangogi <r.arango@uniandes.edu.co>
2022-04-28 07:39:54 -0500

    add db vars as env vars

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,17 @@
+import imp
 import os
 from flask import Flask
-from sqlalchemy import false
+
+
+db_host = os.getenv('DB_HOST', 'rds-blacklist.cco378ibyevv.us-east-1.rds.amazonaws.com')
+db_port = os.getenv('DB_PORT', 5432)
+db_name = os.getenv('DB_NAME', 'blacklist')
+db_user = os.getenv('DB_USER', 'postgres')
+db_password = os.getenv('DB_PASSWORD', 'postgres')
 
 def create_app():
     app = Flask(__name__)    
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@rds-blacklist.cco378ibyevv.us-east-1.rds.amazonaws.com/blacklist'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.config['JWT_SECRET_KEY']='blacklist'
