9196ca438905c8ac992a931d7803fea42d7c3fe4
vquirozuni <v.quiroz@uniandes.edu.co>
2022-04-29 00:58:30 -0500

    UnitTest Listo

--- tests/test_vista_black_list.py
+++ tests/test_vista_black_list.py
@@ -1,9 +1,19 @@
+from email import header
 import unittest
+import json
 from application import application
 from src.modelos.modelos import *
 
 
-
+def login_user(self, username, password):
+        return self.client.post(
+        '/auth/login',
+        data=json.dumps(dict(
+            username=username,
+            password=password
+        )),
+        content_type='application/json',
+    )
 
 class TestVistaBlackList(unittest.TestCase):
     def setUp(self):        
@@ -14,19 +24,53 @@ class TestVistaBlackList(unittest.TestCase):
         self.client = application.test_client()
         db.create_all()
 
-    def test_vista_black_list_get_positive(self):
-        request = self.client.get('/blacklists')
-        code = request.status_code
-        #response = request.get_json()
-        self.assertEqual(code, 200)
+
+    
+
+    def test_vista_black_list_get_all_positive(self):
+        with self.client:
+            token = login_user(self, 'vquiroz', '1234')
+            response = self.client.get(
+                '/blacklists', 
+                headers=dict(
+                    Authorization = 'Bearer ' + json.loads(
+                        token.data.decode()
+                    )['token']
+                )
+            )
+            #data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
 
     def test_vista_blacklist_post_positive(self):
-        new_bloqueado = {
-            "email": "correoprueba@uniandes.edu.co",
-            "app_uuid": "1234dsfsdf9abcdef",
-            "blocked_reason": "Bloqueo de pruebas"
-        }
-        request = self.client.post("/blacklists", json=new_bloqueado)
-        code = request.status_code
-        #response = request.get_json()
-        self.assertEqual(code, 200)
+        with self.client:
+            token = login_user(self, 'vquiroz', '1234')
+            new_bloqueado = {
+                "email": "prueba@uniandes.edu.co",
+                "app_uuid": "sdd1234dsfsdf9abcdef",
+               "blocked_reason": "Bloqueo de test"
+            }
+            response = self.client.post(
+                '/blacklists', json=new_bloqueado, 
+                headers=dict(
+                    Authorization = 'Bearer ' + json.loads(
+                        token.data.decode()
+                    )['token']
+                )
+            )            
+            code = response.status_code
+            self.assertEqual(code, 200)
+
+    def test_vista_black_list_get_one_positive(self):
+        with self.client:
+            token = login_user(self, 'vquiroz', '1234')
+            response = self.client.get(
+                '/blacklists/correoprueba@uniandes.edu.co', 
+                headers=dict(
+                    Authorization = 'Bearer ' + json.loads(
+                        token.data.decode()
+                    )['token']
+                )
+            )
+            data = json.loads(response.data.decode())            
+            self.assertTrue(data['email'] == 'correoprueba@uniandes.edu.co')
+            self.assertEqual(response.status_code, 200)
